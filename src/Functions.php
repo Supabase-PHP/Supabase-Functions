36f48e150811c4eedc9fe1029f3b8eca734e0cce
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-04-17 10:16:56 +0530

    fix Types

--- src/Functions.php
+++ src/Functions.php
@@ -6,8 +6,28 @@ namespace Supabase\Functions;
 
 class Functions
 {
+    /**
+     * @param  string  $url  Supabase URL
+     * @param  string  $token  Supabase Token
+     */
     public function __construct(
-        private string $url,
-        private string $token
+        private readonly string $url,
+        private readonly string $token
     ) {}
+
+    /**
+     * @return array<string, string>
+     */
+    public function getHeaders(): array
+    {
+        return [
+            'Authorization' => 'Bearer '.$this->token,
+            'apikey' => $this->token,
+        ];
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
 }
